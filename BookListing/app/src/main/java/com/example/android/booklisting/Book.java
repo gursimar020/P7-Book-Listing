38d2fd80ec0f3adce8b4e1aa4aa0370a0992ec21
Gursimar Singh <gursimarsingh020@gmail.com>
2017-08-13 10:58:44 +0530

    This repository contain the project I made in Android Basics Nanodegree by Udacity.

---
+++ BookListing/app/src/main/java/com/example/android/booklisting/Book.java
@@ -0,0 +1,12 @@
+package com.example.android.booklisting;
+
+public class Book {
+
+    public final String title; //title
+    public final String author;  //author
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+}
